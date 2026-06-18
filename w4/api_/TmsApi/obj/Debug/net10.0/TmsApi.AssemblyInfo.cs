661e945ad82f1001b10c6335c62a6ebfe84ec5bf
isaac <isaacalex5371@gmail.com>
2026-06-18 10:43:04 +0300

    extendedExtended Exercise (Stretch): Wir Assessment and Certificate into the Database

--- w4/api_/TmsApi/obj/Debug/net10.0/TmsApi.AssemblyInfo.cs
+++ w4/api_/TmsApi/obj/Debug/net10.0/TmsApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TmsApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c7d82b697e189245cd3ac650f3bab9d23d7226b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3ee03d462e1b0615048a5df55980c62cd9c687f")]
 [assembly: System.Reflection.AssemblyProductAttribute("TmsApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TmsApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
